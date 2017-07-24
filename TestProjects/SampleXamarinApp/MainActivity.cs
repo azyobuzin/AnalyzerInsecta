7e90fafbae8ca204efcb0d0a2200f680bd98a32e
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-07-24 17:54:45 +0900

    Add Xamarin.Android project for testing

---
+++ TestProjects/SampleXamarinApp/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+
+namespace SampleXamarinApp
+{
+    [Activity(Label = "SampleXamarinApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+            Console.WriteLine($"This class name is {nameof(MainActivity)}.");
+            Console.WriteLine($"The namespace name is {nameof(SampleXamarinApp)}.");
+        }
+    }
+}
